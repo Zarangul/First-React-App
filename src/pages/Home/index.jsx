854f2eaf7bc2611cf7965c370fe71f3c7d964bc8
Zarangul <zarangulbabazade@gmail.com>
2023-03-16 15:46:11 +0400

    first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import './Home.css'
+import style from './Home.module.css'
+function Home() {
+  return (
+    <div className={style.home}>
+      Home
+    </div>
+  )
+}
+
+export default Home
